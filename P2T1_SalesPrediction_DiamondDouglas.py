35edf6346e381999c76a3612f2082facafe76eb4
2842524 <43898103+2842524@users.noreply.github.com>
2018-10-05 22:39:48 -0400

    Add files via upload
    First submission

---
+++ P2T1_SalesPrediction_DiamondDouglas.py
@@ -0,0 +1,13 @@
+# Exercise 2 on page 77
+# October 5 2018
+# CTI-110 P2T1 - Sales Prediction
+# Diamond Douglas
+
+
+total_sales = float(input("Please enter the projected amount" +\
+                                " of total sales: "))
+profit = total_sales * 0.23
+print('The profit is $', format(profit, ',.2f'))
+
+
+
